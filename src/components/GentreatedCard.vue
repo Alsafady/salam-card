50312803bd499e7651caadaa24f8e0858e46399e
Alsafady <15532037+Alsafady@users.noreply.github.com>
2023-04-20 05:24:18 +0300

    3 files modified, 3 added, 3 deleted

--- src/components/GentreatedCard.vue
+++ src/components/GentreatedCard.vue
@@ -1,121 +1,129 @@
 <template>
   Horizontal Card: Recommended for snapchat, whatsapp status, instagram stories
-<div class="card-area-h " id="card-area-h" >
-   <img src="/assets/card_h.png" alt="" width="357" height="625" class="d-inline-block pt-n5">
-   <div class="empNameH">
-        {{ empName}}
-   </div>
-<br>
-    <div class="empNameEnH">
-       {{empNameEn}}
-   </div>
-</div>
-
-    <br>
-
-    <div class="mt-4 ">
-        <button type="button" class="btn btn-secondary btn-lg" @click="generateCardH()">تــحميــل</button>
+  <div class="card-area-h" id="card-area-h">
+    <img
+      src="/public/assets/card_h.png"
+      alt=""
+      width="357"
+      height="625"
+      class="d-inline-block pt-n5"
+    />
+    <div class="empNameH">
+      {{ empName }}
     </div>
+    <br />
+    <div class="empNameEnH">
+      {{ empNameEn }}
+    </div>
+  </div>
 
-    <br>
-    <hr>
-    <br>
+  <br />
+
+  <div class="mt-4">
+    <button
+      type="button"
+      class="btn btn-secondary btn-lg"
+      @click="generateCardH()"
+    >
+      تــحميــل
+    </button>
+  </div>
+
+  <br />
+  <hr />
+  <br />
 
   Vertical Card: Recommended for linkedin, Twitter, facebook
 
-    <div class="card-area-v " id="card-area-v" >
-        <img src="/assets/card_v.png" alt="" width="600" height="315" class="d-inline-block pt-n5">
-        <div class="empNameV">
-            {{ empName}}
-        </div>
-        <br>
-        <div class="empNameEnV">
-            {{empNameEn}}
-        </div>
+  <div class="card-area-v" id="card-area-v">
+    <img
+      src="/assets/card_v.png"
+      alt=""
+      width="600"
+      height="315"
+      class="d-inline-block pt-n5"
+    />
+    <div class="empNameV">
+      {{ empName }}
     </div>
+    <br />
+    <div class="empNameEnV">
+      {{ empNameEn }}
+    </div>
+  </div>
 
-<div class="mt-4 ">
-    <button type="button" class="btn btn-secondary btn-lg" @click="generateCardV()">تــحميــل</button>
-</div>
-
-
+  <div class="mt-4">
+    <button
+      type="button"
+      class="btn btn-secondary btn-lg"
+      @click="generateCardV()"
+    >
+      تــحميــل
+    </button>
+  </div>
 </template>
 <script>
 import html2canvas from 'html2canvas';
 
-
 export default {
   name: 'GentreatedCard',
-  props:{
-    empName:String,
-    empNameEn:String,
+  props: {
+    empName: String,
+    empNameEn: String,
   },
-   methods:{
-      async generateCardH() {
-        const canvas = await html2canvas(document.getElementById("card-area-h"));
-        canvas.style.display = "none";
-        document.body.appendChild(canvas);
-        const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-        const a = document.createElement("a");
+  methods: {
+    async generateCardH() {
+      const canvas = await html2canvas(document.getElementById('card-area-h'));
+      canvas.style.display = 'none';
+      document.body.appendChild(canvas);
+      const image = canvas
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream');
+      const a = document.createElement('a');
 
-        a.setAttribute("download", `My-Salam-Hcard.png`);
-        a.setAttribute("href", image);
-        a.click();
+      a.setAttribute('download', `My-Salam-Hcard.png`);
+      a.setAttribute('href', image);
+      a.click();
+    },
 
-      },
+    async generateCardV() {
+      const canvas = await html2canvas(document.getElementById('card-area-v'));
+      canvas.style.display = 'none';
+      document.body.appendChild(canvas);
+      const image = canvas
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream');
+      const a = document.createElement('a');
 
-
-       async generateCardV() {
-           const canvas = await html2canvas(document.getElementById("card-area-v"));
-           canvas.style.display = "none";
-           document.body.appendChild(canvas);
-           const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-           const a = document.createElement("a");
-
-           a.setAttribute("download", `My-Salam-Vcard.png`);
-           a.setAttribute("href", image);
-           a.click();
-
-       }
-
-}
-
-
-
-
-    
-   }
-   
-
-
-
+      a.setAttribute('download', `My-Salam-Vcard.png`);
+      a.setAttribute('href', image);
+      a.click();
+    },
+  },
+};
 </script>
 <style>
-
-
-.card-area-h{
+.card-area-h {
   position: relative;
   text-align: center;
   width: 357px;
-  height:625px;
-    margin-left: auto;
-    margin-right: auto;
-
+  height: 625px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
-.card-area-v{
-    position: relative;
-    text-align: center;
-    width: 600px;
-    height:315px;
+.card-area-v {
+  position: relative;
+  text-align: center;
+  width: 600px;
+  height: 315px;
 }
 
+.empNameH {
+  font-family: 'Tajawal', sans-serif !important;
+  font-size: 20px;
 
-.empNameH{
-font-family:  'Tajawal', sans-serif !important;
-font-size: 20px;
-
-position: absolute;
+  position: absolute;
   top: 57%;
   /*left: 35%;*/
   width: 100%;
@@ -123,45 +131,41 @@ position: absolute;
   color: #002035 !important;
   /* transform: translate(-50%, -50%); */
 }
-.empNameV{
-    font-family:  'Tajawal', sans-serif !important;
-    font-size: 20px;
+.empNameV {
+  font-family: 'Tajawal', sans-serif !important;
+  font-size: 20px;
 
-    position: absolute;
-    top: 45%;
-    right: 4%;
-    width: 100%;
+  position: absolute;
+  top: 45%;
+  right: 4%;
+  width: 100%;
 
-    color: #002035 !important;
-    /* transform: translate(-50%, -50%); */
+  color: #002035 !important;
+  /* transform: translate(-50%, -50%); */
 }
 
+.empNameEnH {
+  font-family: 'Tajawal', sans-serif !important;
+  font-size: 20px;
 
-.empNameEnH{
-    font-family:  'Tajawal', sans-serif !important;
-    font-size: 20px;
+  position: absolute;
+  top: 84%;
+  /*right: 4%;*/
+  width: 100%;
 
-    position: absolute;
-    top: 84%;
-    /*right: 4%;*/
-    width: 100%;
-
-    color: #002035 !important;
-    /* transform: translate(-50%, -50%); */
+  color: #002035 !important;
+  /* transform: translate(-50%, -50%); */
 }
-.empNameEnV{
-    font-family:  'Tajawal', sans-serif !important;
-    font-size: 20px;
+.empNameEnV {
+  font-family: 'Tajawal', sans-serif !important;
+  font-size: 20px;
 
-    position: absolute;
-    top: 77%;
-    right: 4%;
-    width: 100%;
+  position: absolute;
+  top: 77%;
+  right: 4%;
+  width: 100%;
 
-    color: #002035 !important;
-    /* transform: translate(-50%, -50%); */
+  color: #002035 !important;
+  /* transform: translate(-50%, -50%); */
 }
-
-
-
 </style>
